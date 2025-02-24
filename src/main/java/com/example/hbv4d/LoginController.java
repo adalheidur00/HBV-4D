e6ed5deb572fe2d6942e79062bb6dea27d6b10bb
adalheidur00 <147988552+adalheidur00@users.noreply.github.com>
2025-02-24 09:16:50 +0000

    Tours, Login

---
+++ src/main/java/com/example/hbv4d/LoginController.java
@@ -0,0 +1,6 @@
+package com.example.hbv4d;
+public class LoginController {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/example/hbv4d/ToursController.java
@@ -0,0 +1,6 @@
+package com.example.hbv4d;
+public class ToursController {
+    public static void main(String[] args) {
+        
+    }
+}
