ef8663cae84bb1971731fbcc445c030981d517b7
adalheidur00 <147988552+adalheidur00@users.noreply.github.com>
2025-02-24 09:46:57 +0000

    bæta við javadoc

--- src/main/java/com/example/hbv4d/IndexController.java
+++ src/main/java/com/example/hbv4d/IndexController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 
 public class IndexController {
 
+    /**
+     * Færir user í browseTour senu
+     * @throws Exception
+     */
     @FXML
     public void onBrowseTours() throws Exception{
         Application.switchScene("browseTour-view.fxml");
